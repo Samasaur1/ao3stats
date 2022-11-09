d9be94435e4dd02ee68559aa74b35aec9f0875f7
Samasaur1 <30577766+Samasaur1@users.noreply.github.com>
2022-11-09 14:58:26 -0800

    Overhaul parsing and fix output CSV

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import NoSuchElementException
 
 usr = input("Username? ")
 
@@ -19,7 +20,7 @@ input("Please sign in")
 driver.get(f"https://archiveofourown.org/users/{usr}/readings")
 
 last_page = int(input("Number of pages? "))
-print(last_page)
+# print(last_page)
 
 uniq_fic_count = 0
 fic_count = 0
@@ -36,52 +37,70 @@ deleted_works = 0
 
 authors = {}
 
-rl = WebDriverWait(driver, timeout=3).until(lambda d: d.find_element(By.CSS_SELECTOR, "#main ol.reading"))
-for work_number in range(1,21):
-    #work = rl.find_element(By.CSS_SELECTOR, f":nth-child({work_number})")
-    work = driver.find_element(By.CSS_SELECTOR, f"#main ol.reading li.work:nth-of-type({work_number})")
-    #print(work.text)
+def process_page():
+    WebDriverWait(driver, timeout=3).until(lambda d: d.find_element(By.CSS_SELECTOR, "#main ol.reading"))
+    works = driver.find_elements(By.CSS_SELECTOR, f"#main > ol.reading > li.work")
+    for work in works:
+        process_work(work)
+
+def process_work(work):
+    global uniq_fic_count
+    global fic_count
+    global uniq_word_count
+    global word_count
+    global most_read_fic
+    global most_reads
+    global longest_fic
+    global longest_length
+    global marked_for_later
+    global deleted_works
+    global authors
     lines = work.text.split('\n')
     if len(lines) == 1 and "Deleted work" in lines[0]:
         deleted_works += 1
-        continue
-    stats = lines[-2]
+        return
     visits = lines[-1]
-    #print(stats)
-    #print(visits)
     if "(Marked for Later.)" in visits:
         marked_for_later += 1
-        continue
+        return
     if visits[-4:] == "once":
         vc = 1
     else:
         words = visits.split(' ')
         vc = int(words[-2])
-    #print(vc)
-    idx = stats.find("Words: ")
-    if idx == -1:
-        continue
-    chopped = stats[idx + 7:].split(' ')
-    _wc = chopped[0]
+    _authors = work.find_elements(By.CSS_SELECTOR, "div.header.module > h4.heading > a[rel=\"author\"]")
+    if len(_authors) == 0:
+        author = "Anonymous"
+    else:
+        author = " ".join([x.text for x in _authors])
+    try:
+        fic_name = work.find_element(By.CSS_SELECTOR, "div.header.module > h4.heading > a[href^=\"/works/\"]").text
+    except NoSuchElementException:
+        print("no fic name?")
+        print(lines)
+        return
+    giftees = work.find_elements(By.CSS_SELECTOR, "div.header.module > h4.heading > a[href$=\"/gifts\"]")
+    print(lines[0])
+    print(author)
+    print(fic_name)
+    print(giftees)
+    _wc = work.find_element(By.CSS_SELECTOR, "dl.stats > dd.words").text
     wc = int(_wc.replace(",", ""))
-    #print(wc)
     uniq_fic_count += 1
     fic_count += vc
     uniq_word_count += wc
     word_count += vc * wc
 
-    author = lines[0].split(" ")[-1]
-    #fic_name = lines[0].split(" ")[0:-2]
     _fics, _uniq_fics, _words, _uniq_words, _most_read_fic, _times_read, _longest_fic, _longest_length = authors.get(author, (0,0,0,0,"",0,"",0))
     if vc > _times_read:
         _ntr = vc
-        _nmrf = lines[0]
+        _nmrf = f"{fic_name} by {author}"
     else:
         _ntr = _times_read
         _nmrf = _most_read_fic
     if wc > _longest_length:
         _nll = wc
-        _nlf = lines[0]
+        _nlf = f"{fic_name} by {author}"
     else:
         _nll = _longest_length
         _nlf = _longest_fic
@@ -89,72 +108,18 @@ for work_number in range(1,21):
 
     if vc > most_reads:
         most_reads = vc
-        most_read_fic = lines[0]
+        most_read_fic = f"{fic_name} by {author}"
     if wc > longest_length:
         longest_length = wc
-        longest_fic = lines[0]
+        longest_fic = f"{fic_name} by {author}"
+
+
+process_page()
 
 for i in range(2, last_page + 1):
     print(f"processing page {i}")
     driver.get(f"https://archiveofourown.org/users/{usr}/readings?page={i}")
-    WebDriverWait(driver, timeout=3).until(lambda d: d.find_element(By.CSS_SELECTOR, "#main ol.reading"))
-    works = driver.find_elements(By.CSS_SELECTOR, f"#main > ol.reading > li.work")
-    for work in works:
-        #print(work.text)
-        lines = work.text.split('\n')
-        if len(lines) == 1 and "Deleted work" in lines[0]:
-            deleted_works += 1
-            continue
-        stats = lines[-2]
-        visits = lines[-1]
-        #print(stats)
-        #print(visits)
-        if "(Marked for Later.)" in visits:
-            marked_for_later += 1
-            continue
-        if visits[-4:] == "once":
-            vc = 1
-        else:
-            words = visits.split(' ')
-            vc = int(words[-2])
-        #print(vc)
-        idx = stats.find("Words: ")
-        if idx == -1:
-            continue
-        chopped = stats[idx + 7:].split(' ')
-        _wc = chopped[0]
-        wc = int(_wc.replace(",", ""))
-        #print(wc)
-        uniq_fic_count += 1
-        fic_count += vc
-        uniq_word_count += wc
-        word_count += vc * wc
-
-        splline1 = lines[0].split(" ")
-        author = splline1[-1] if splline1[-2] == "by" else splline1[-3]
-        #fic_name = lines[0].split(" ")[0:-2]
-        _fics, _uniq_fics, _words, _uniq_words, _most_read_fic, _times_read, _longest_fic, _longest_length = authors.get(author, (0,0,0,0,"",0,"",0))
-        if vc > _times_read:
-            _ntr = vc
-            _nmrf = lines[0]
-        else:
-            _ntr = _times_read
-            _nmrf = _most_read_fic
-        if wc > _longest_length:
-            _nll = wc
-            _nlf = lines[0]
-        else:
-            _nll = _longest_length
-            _nlf = _longest_fic
-        authors[author] = (_fics + vc, _uniq_fics + 1, _words + vc * wc, _uniq_words + wc, _nmrf, _ntr, _nlf, _nll)
-
-        if vc > most_reads:
-            most_reads = vc
-            most_read_fic = lines[0]
-        if wc > longest_length:
-            longest_length = wc
-            longest_fic = lines[0]
-
+    process_page()
 
     print(f"uniq_fic_count: {uniq_fic_count}")
     print(f"fic_count: {fic_count}")
@@ -193,3 +158,9 @@ with open("authors.csv", "w") as file:
 print(authors)
 
 driver.quit()
+
+# Word count: `li.work > dl.stats > dd.words` (must parse to int)
+# author: `li.work > div.header.module > h4.heading > a[rel="author"]` (str, optionally ensure href does not have gift)
+# fic name: `li.work > div.header.module > h4.heading > a[href^="/works/"]` (str)
+# giftee(s?): `li.work > div.header.module > h4.heading > a[href$="/gifts"]` (str)
+# my first page loop and second page loop are almost identical. revise 1st page loop to actually loop over work elements, not hardcoded 1...20
